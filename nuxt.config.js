86ef194e932d74e1c9821d3ac9037c2f3489a1fe
fabianbeier <fabianbeier@online.de>
2021-11-18 16:59:44 +0100

    Fade in

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,29 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio200',
+    title: "portfolio200",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-
-    { src: '~/plugins/spline.js', mode: 'client' }, // only on client side
-
+    { src: "~/plugins/spline.js", mode: "client" }, // only on client side
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -39,15 +32,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content",
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
@@ -55,15 +48,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-
-
-    extend: function (config, {isDev, isClient}) {
-
-        config.node = {
-
-            fs: "empty"
-        };
-    }
-},
-
-}
+    extend: function (config, { isDev, isClient }) {
+      config.node = {
+        fs: "empty",
+      };
+    },
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="show">
     <div class="p-10 flex h-screen flex-col justify-between z-10">
       <div class="top-row flex justify-between">
         <h1>Fabian Beier<br />Design & Development</h1>
@@ -8,26 +8,65 @@
             circle
             w-24
             h-24
-            border-2 border-indigo-200
+            border-2 border-green-500
             rounded-full
             flex
             justify-center
             items-center
           "
         >
-          <p class="mb-1">Contact</p>
+          <transition name="page" appear>
+            <p class="mb-1">Contact</p>
+          </transition>
         </div>
       </div>
-      <div class="flex gap-2 w-3/12">
-        <nuxt-content :document="link" class=""/>
+      <div class="flex gap-2 w-3/12 text-3xl leading-7">
+        <nuxt-content :document="link" class="" />
       </div>
       <div class="lower-row flex justify-between">
         <div class="w-5/12 text-3xl leading-none">
-          <ul>
-            <li v-for="site in sites" :key="site.slug" class="group m-0 p-0 ">
-              <a :href="site.link" target="blank" @mouseover="updateLink(site)">{{ site.title }}<span class=" uppercase tracking-widest transition-all w-0 overflow-hidden opacity-0 inline-block text-base align-top group-hover:w-10  group-hover:opacity-100 ml-2">open</span><span class=" uppercase tracking-widest opacity-30 text-base align-top m-2">{{site.task}}</span></a>
-            </li>
-          </ul>
+         
+            <transition-group
+              appear
+              name="list"
+              tag="ul"
+            >
+              <li v-for="(site) in sites" :key="site.slug" class="group m-0 p-0">
+                <a
+                  :href="site.link"
+                  target="blank"
+                  @mouseover="updateLink(site)"
+                  >{{ site.title
+                  }}<span
+                    class="
+                      uppercase
+                      tracking-widest
+                      transition-all
+                      w-0
+                      overflow-hidden
+                      opacity-0
+                      inline-block
+                      text-base
+                      align-top
+                      group-hover:w-10 group-hover:opacity-100
+                      ml-2
+                    "
+                    >open</span
+                  ><span
+                    class="
+                      uppercase
+                      tracking-widest
+                      opacity-30
+                      text-base
+                      align-top
+                      m-2
+                    "
+                    >{{ site.task }}</span
+                  ></a
+                >
+              </li>
+            </transition-group>
+     
         </div>
         <div class="w-5/12 flex flex-col justify-between h-100">
           <div class="flex grid-cols-2 gap-2">
@@ -44,11 +83,11 @@
               ab voluptas? Voluptate natus adipisci eveniet.
             </p>
           </div>
-          <div class=" opacity-30">Impressum, Datenschutz</div>
+          <div class="opacity-30">Impressum, Datenschutz</div>
         </div>
       </div>
     </div>
-    <div class="w-full fixed h-full top-0 left-0 -z-10 opacity-80">
+    <div class="w-full fixed h-full top-0 left-0 -z-10 opacity-40">
       <div id="container">
         <canvas id="canvas3d"></canvas>
       </div>
@@ -57,38 +96,77 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
   async asyncData({ $content }) {
-    const sites = await $content('sites').fetch()
+    const sites = await $content("sites").fetch();
     return {
       sites,
-    }
+    };
   },
   data() {
     return {
-      link: null
-    }
+      link: null,
+      show: false,
+    };
   },
   methods: {
     updateLink(url: any) {
       this.link = url;
-      console.log(url)
-    }
+      console.log(url);
+    },
   },
-})
+  transition: {
+    afterEnter(el) {
+      console.log("afterLeave", el);
+    },
+  },
+  mounted() {
+    this.show = true;
+  },
+});
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@500&display=swap");
 
 h1 {
-  @apply text-lg leading-none tracking-wider;
+  @apply leading-none tracking-wider;
 }
 h2 {
   @apply text-xl;
 }
 body {
-  @apply bg-black  text-indigo-200 font-grotesk text-base leading-none tracking-wider;
+  @apply bg-black  text-green-500 font-grotesk text-base leading-none tracking-wider;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: opacity 0.5s;
+}
+.layout-enter,
+.layout-leave-active {
+  opacity: 0;
+}
+
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 5s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
